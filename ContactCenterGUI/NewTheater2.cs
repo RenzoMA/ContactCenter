18be3621d85fbea0273a890976b096579e83ae56
Renzo Maldonado <renzoma89@gmail.com>
2016-04-22 01:30:48 -0500

    Avance Match Asiento vs Boton

--- ContactCenterGUI/NewTheater2.cs
+++ ContactCenterGUI/NewTheater2.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using MaterialSkin.Animations;
 using MaterialSkin.Controls;
+using ContactCenterBE.CC.TH.Entidades.AsientoBE;
 
 
 namespace ContactCenterGUI
@@ -28,6 +29,31 @@ namespace ContactCenterGUI
         private void label2_Click(object sender, EventArgs e)
         {
             this.WindowState = FormWindowState.Minimized;
+        }
+        private Asiento GetAsiento(int IdAsiento,List<Asiento> lista)
+        {
+            
+            foreach (Asiento asiento in lista)
+            {
+                if (asiento.IdAsiento == IdAsiento)
+                {
+                    return asiento;
+                }
+            }
+            return null;
+        }
+
+        private void CruzarBotonData(List<Asiento> lAsiento)
+        {
+            foreach (Button btn in this.Controls.OfType<Button>())
+            {
+                int IdAsiento;
+                bool isNumber = int.TryParse(btn.Name, out IdAsiento);
+                if (isNumber)
+                {
+                    btn.Tag = GetAsiento(IdAsiento, lAsiento);
+                }
+            }
         }
     }
 }
