45bbe56c6bc46e29e2b36c194d47589aae09b2ca
AtoVader <AtoVader@ALONSO>
2016-04-23 19:40:53 -0500

    ClienteDA

--- ContactCenterDA/Repositories/CC/ClienteRepository.cs
+++ ContactCenterDA/Repositories/CC/ClienteRepository.cs
@@ -14,185 +14,104 @@ namespace ContactCenterDA.Repositories.CC
     {
         OleDbConnection cnx = new OleDbConnection();
         OleDbCommand cmd = new OleDbCommand();
-        OleDbDataReader dtr = default(OleDbDataReader);
 
         public void Delete(int id)
         {
-            try
-            {
-                cnx.ConnectionString = cnx.GetConexion();
-                cmd.CommandType = CommandType.Text;
-                string sql = String.Format("DELETE FROM CC_CLIENTE WHERE IdCliente = {0}",
-                                            id);
-                cmd.CommandText = sql;
-                cnx.Open();
-                cmd.ExecuteNonQuery();
-            }
-            catch (OleDbException ex)
-            {
-                throw new Exception(ex.Message);
-            }
-            finally
-            {
-                if (cnx.State == ConnectionState.Open)
-                {
-                    cnx.Close();
-                }
-            }
+            String sql = "SELECT * FROM CC_Cliente WHERE = @codigo";
+
+            OleDbParameter codigo = UtilDA.SetParameters("@codigo", OleDbType.Integer, id);
+            UtilDA.ExecuteNonQuery(cmd, CommandType.Text, sql, cnx, codigo);
+
         }
 
         public Cliente GetById(int id)
         {
             Cliente objCliente = null;
 
-            try
-            {
-                cnx.ConnectionString = cnx.GetConexion();
-                cmd.Connection = cnx;
-                cmd.CommandType = CommandType.Text;
-                string sql = String.Format("SELECT * FROM CC_CLIENTE WHERE IdCliente = {0}", id);
-                cmd.CommandText = sql;
-                cnx.Open();
-                dtr = cmd.ExecuteReader();
-                if (dtr.HasRows)
-                {
-                    dtr.Read();
+            String sql = "SELECT * FROM CC_Cliente WHERE IdAplicacion = @idCliente";
 
+            OleDbParameter codigo = UtilDA.SetParameters("@idCliente", OleDbType.Integer, id);
+
+            using (var dtr = UtilDA.ExecuteReader(cmd, CommandType.Text, sql, cnx, codigo))
+            {
+                while (dtr.Read())
+                {
                     objCliente = new Cliente();
-                    objCliente.IdCliente = Convert.ToInt32(dtr.GetValue(dtr.GetOrdinal("IdCliente")).ToString());
-                    objCliente.Nombre = dtr.GetValue(dtr.GetOrdinal("Nombre")).ToString();
-                    objCliente.ApellidoPaterno = dtr.GetValue(dtr.GetOrdinal("ApePaterno")).ToString();
-                    objCliente.Apellidomaterno = dtr.GetValue(dtr.GetOrdinal("ApeMaterno")).ToString();
-                    objCliente.Telefono = dtr.GetValue(dtr.GetOrdinal("Telefono")).ToString();
-                    objCliente.Correo = dtr.GetValue(dtr.GetOrdinal("Correo")).ToString();
-                    objCliente.FechaCreacion = Convert.ToDateTime(dtr.GetValue(dtr.GetOrdinal("FechaCrea")).ToString());
-                    objCliente.UsuarioCreacion = dtr.GetValue(dtr.GetOrdinal("UsuarioCrea")).ToString();
-                    objCliente.FechaModificacion = Convert.ToDateTime(dtr.GetValue(dtr.GetOrdinal("FechaMod")).ToString());
-                    objCliente.UsuarioModificacion = dtr.GetValue(dtr.GetOrdinal("UsuarioMod")).ToString();
+                    objCliente.IdCliente = DataConvert.ToInt(dtr["IdCliente"]);
+                    objCliente.Nombre = DataConvert.ToString(dtr["Nombre"]);
+                    objCliente.ApellidoPaterno = DataConvert.ToString(dtr["ApePaterno"]);
+                    objCliente.Apellidomaterno = DataConvert.ToString(dtr["ApeMaterno"]);
+                    objCliente.Telefono = DataConvert.ToString(dtr["Telefono"]);
+                    objCliente.FechaCreacion = DataConvert.ToDateTime(dtr["FechaCrea"]);
+                    objCliente.UsuarioCreacion = DataConvert.ToString(dtr["UserCrea"]);
+                    objCliente.FechaModificacion = DataConvert.ToDateTime(dtr["FechaMod"]);
+                    objCliente.UsuarioModificacion = DataConvert.ToString(dtr["UserMod"]);
 
-                    dtr.Close();
-                }
-
-                return objCliente;
-            }
-            catch (OleDbException ex)
-            {
-                throw new Exception(ex.Message);
-            }
-            finally
-            {
-                if (cnx.State == ConnectionState.Open)
-                {
-                    cnx.Close();
                 }
             }
+            UtilDA.Close(cnx);
+            return objCliente;
         }
 
         public IList<Cliente> GetLista()
         {
-            List<Cliente> listaCliente = null;
+            List<Cliente> listaCliente = new List<Cliente>();
 
-            try
+            String sql = "SELECT * FROM CC_Cliente";
+
+            using (var dtr = UtilDA.ExecuteReader(cmd, CommandType.Text, sql, cnx))
             {
-                cnx.ConnectionString = cnx.GetConexion();
-                cmd.Connection = cnx;
-                cmd.CommandType = CommandType.Text;
-                string sql = String.Format("SELECT * FROM CC_CLIENTE");
-                cmd.CommandText = sql;
-                cnx.Open();
-                dtr = cmd.ExecuteReader();
-                if (dtr.HasRows)
+                while (dtr.Read())
                 {
-                    while (dtr.Read())
-                    {
-                        Cliente objCliente = new Cliente();
-                        objCliente.IdCliente = Convert.ToInt32(dtr.GetValue(dtr.GetOrdinal("IdCliente")).ToString());
-                        objCliente.Nombre = dtr.GetValue(dtr.GetOrdinal("Nombre")).ToString();
-                        objCliente.ApellidoPaterno = dtr.GetValue(dtr.GetOrdinal("ApePaterno")).ToString();
-                        objCliente.Apellidomaterno = dtr.GetValue(dtr.GetOrdinal("ApeMaterno")).ToString();
-                        objCliente.Correo = dtr.GetValue(dtr.GetOrdinal("Correo")).ToString();
-                        objCliente.Telefono = dtr.GetValue(dtr.GetOrdinal("Telefono")).ToString();
-                        objCliente.FechaCreacion = Convert.ToDateTime(dtr.GetValue(dtr.GetOrdinal("FechaCrea")).ToString());
-                        objCliente.UsuarioCreacion = dtr.GetValue(dtr.GetOrdinal("UsuarioCrea")).ToString();
-                        objCliente.FechaModificacion = Convert.ToDateTime(dtr.GetValue(dtr.GetOrdinal("FechaMod")).ToString());
-                        objCliente.UsuarioModificacion = dtr.GetValue(dtr.GetOrdinal("UsuarioMod")).ToString();
-                        listaCliente.Add(objCliente);
-                    }
-
-                    dtr.Close();
-                }
-
-
-                return listaCliente;
-            }
-            catch (OleDbException ex)
-            {
-                throw new Exception(ex.Message);
-            }
-            finally
-            {
-                if (cnx.State == ConnectionState.Open)
-                {
-                    cnx.Close();
+                    Cliente objCliente = new Cliente();
+                    objCliente.IdCliente = DataConvert.ToInt(dtr["IdCliente"]);
+                    objCliente.Nombre = DataConvert.ToString(dtr["Nombre"]);
+                    objCliente.ApellidoPaterno = DataConvert.ToString(dtr["ApePaterno"]);
+                    objCliente.Apellidomaterno = DataConvert.ToString(dtr["ApeMaterno"]);
+                    objCliente.Telefono = DataConvert.ToString(dtr["Telefono"]);
+                    objCliente.Correo = DataConvert.ToString(dtr["Correo"]);
+                    objCliente.FechaCreacion = DataConvert.ToDateTime(dtr["FechaCrea"]);
+                    objCliente.UsuarioCreacion = DataConvert.ToString(dtr["UserCrea"]);
+                    objCliente.FechaModificacion = DataConvert.ToDateTime(dtr["FechaMod"]);
+                    objCliente.UsuarioModificacion = DataConvert.ToString(dtr["UserMod"]);
+                    listaCliente.Add(objCliente);
                 }
             }
+            UtilDA.Close(cnx);
+            return listaCliente;
         }
 
         public void Insert(Cliente datos)
         {
 
-            cnx.ConnectionString = cnx.GetConexion();
-            cmd.Connection = cnx;
-            cmd.CommandType = CommandType.Text;
-            string sql = String.Format("INSERTO INTO CC_CLIENTE(Nombre, ApePaterno, ApeMaterno, Correo, Telefono, FechaCrea, UsuarioCrea, FechaMod, UsuarioMod) " +
-                                        "values('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}','{8}')",
-                                         datos.Nombre, datos.ApellidoPaterno, datos.Apellidomaterno, datos.Correo, datos.Telefono, datos.FechaCreacion, datos.UsuarioCreacion,
-                                         datos.FechaModificacion, datos.UsuarioModificacion);
-            cmd.CommandText = sql;
-            try
-            {
-                cnx.Open();
-                cmd.ExecuteNonQuery();
-            }
-            catch (OleDbException ex)
-            {
-                throw new Exception(ex.Message);
-            }
-            finally
-            {
-                if (cnx.State == ConnectionState.Open)
-                {
-                    cnx.Close();
-                }
-            }
+            String sql = "INSERT INTO CC_Cliente(Nombre, ApePaterno, ApeMaterno, Correo, Telefono, FechaCrea, UsuarioCrea, FechaCrea) " +
+                                       "values(@nombre,@apePaterno,@apeMaterno,@correo,@telefono,@fechaCrea, @UsuarioCrea)";
+
+            OleDbParameter nombre = UtilDA.SetParameters("@nombre", OleDbType.VarChar, datos.Nombre);
+            OleDbParameter apePaterno = UtilDA.SetParameters("@apePaterno", OleDbType.VarChar, datos.ApellidoPaterno);
+            OleDbParameter apeMaterno = UtilDA.SetParameters("@apeMaterno", OleDbType.VarChar, datos.Apellidomaterno);       
+            OleDbParameter telefeno = UtilDA.SetParameters("@telefono", OleDbType.Date, datos.Telefono);
+            OleDbParameter correo = UtilDA.SetParameters("@correo", OleDbType.VarChar, datos.Correo);
+            OleDbParameter fechaCrea = UtilDA.SetParameters("@fechaCrea", OleDbType.Date, datos.FechaCreacion);
+            OleDbParameter UsuarioCrea = UtilDA.SetParameters("@UsuarioCrea", OleDbType.VarChar, datos.UsuarioCreacion);
+
+            UtilDA.ExecuteNonQuery(cmd, CommandType.Text, sql, cnx, nombre, apePaterno, apeMaterno, telefeno, correo, fechaCrea, UsuarioCrea);
         }
 
         public void Update(Cliente datos)
         {
-            cnx.ConnectionString = cnx.GetConexion();
-            cmd.Connection = cnx;
-            cmd.CommandType = CommandType.Text;
-            String sql = String.Format("UPDATE CC_CLIENTE SET Nombre = {0}, ApePaterno = {1}, ApeMaterno = {2}, ApePaterno = {3}, Correo = {4}" +
-                                        "Telefono = '{5}', FechaCrea = {6}, UsuarioCrea = {7}, FechaMod = {8}, UsuarioMod = {9} where IdCliente = {10}",
-                                        datos.Nombre, datos.ApellidoPaterno, datos.Apellidomaterno, datos.Correo, datos.Telefono, datos.FechaCreacion, datos.UsuarioCreacion,
-                                        datos.FechaModificacion, datos.UsuarioModificacion, datos.IdCliente);
-            cmd.CommandText = sql;
-            try
-            {
-                cnx.Open();
-                cmd.ExecuteNonQuery();
-            }
-            catch (OleDbException ex)
-            {
-                throw new Exception(ex.Message);
-            }
-            finally
-            {
-                if (cnx.State == ConnectionState.Open)
-                {
-                    cnx.Close();
-                }
-            }
+            String sql = "UPDATE CC_Cliente SET Nombre = @nombre, ApePaterno = @apePaterno, ApeMaterno = @apeMaterno, Correo = @correo, " +
+                                       "Telefono = @telefono, FechaMod = @fechaMod, UsuariMod = @usuarioMod";
+
+            OleDbParameter nombre = UtilDA.SetParameters("@nombre", OleDbType.VarChar, datos.Nombre);
+            OleDbParameter apePaterno = UtilDA.SetParameters("@apePaterno", OleDbType.VarChar, datos.ApellidoPaterno);
+            OleDbParameter apeMaterno = UtilDA.SetParameters("@apeMaterno", OleDbType.VarChar, datos.Apellidomaterno);
+            OleDbParameter telefeno = UtilDA.SetParameters("@telefono", OleDbType.Date, datos.Telefono);
+            OleDbParameter correo = UtilDA.SetParameters("@correo", OleDbType.VarChar, datos.Correo);
+            OleDbParameter fechaMod = UtilDA.SetParameters("@fechaCrea", OleDbType.Date, datos.FechaCreacion);
+            OleDbParameter UsuarioMod = UtilDA.SetParameters("@UsuarioCrea", OleDbType.VarChar, datos.UsuarioCreacion);
+
+            UtilDA.ExecuteNonQuery(cmd, CommandType.Text, sql, cnx, nombre, apePaterno, apeMaterno, telefeno, correo, fechaMod, UsuarioMod);
         }
     }
 }
