96f0213e8efd694a899b8811a61b09116a9d2af0
AtoVader <AtoVader@ALONSO>
2016-04-24 14:15:21 -0500

    Clase tarifa añadida

---
+++ ContactCenterBE/CC/TH/Entidades/TarifaBE/Tarifa.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ContactCenterBE.CC.TH.Entidades.ZonaBE;
+using ContactCenterBE.CC.TH.Entidades.ObraBE;
+using ContactCenterBE.Base;
+
+
+namespace ContactCenterBE.CC.TH.Entidades.TarifaBE
+{
+    public class Tarifa:BaseEntity
+    {
+        public int IdTarifa { get; set; }
+
+        public Zona Zona {get; set;}
+
+        public Obra Obra { get; set; }
+
+        public Single Precio { get; set; }
+    }
+}
