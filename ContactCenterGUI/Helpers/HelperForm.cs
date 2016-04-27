bf2dec16ad1ff5579ea31a2e7dfe959cfd6e72ad
Renzo Maldonado <renzoma89@gmail.com>
2016-04-26 22:27:06 -0500

    go

---
+++ ContactCenterGUI/Helpers/HelperForm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ContactCenterGUI.Helpers
+{
+    class HelperForm
+    {
+        public static void changeForm(string formName, string aplicacion, bool hide,Form form, object parametro = null)
+        {
+            Type CAType = Type.GetType("ContactCenterGUI." + aplicacion + "." + formName);
+            if (CAType != null)
+            {
+                if (hide) form.Hide();
+                Form nextForm2 = (Form)Activator.CreateInstance(CAType);
+                nextForm2.Tag = parametro;
+                nextForm2.ShowDialog();
+                if (hide) form.Show();
+            }
+            else
+            {
+                MessageBox.Show("Formulario no encontrado");
+            }
+        }
+    }
+}
