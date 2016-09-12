397093de61a9ffc258d755eac9c014bd36309bdf
Gian <gianfrancocm_06@hotmail.com>
2016-09-12 16:14:25 -0500

    mejora html

--- ContactCenterCommon/Constantes.cs
+++ ContactCenterCommon/Constantes.cs
@@ -11,45 +11,50 @@ namespace ContactCenterCommon
         public static class Mails
         {
             public const string TeatroConfirmacionReserva = "	 <!DOCTYPE html>	" +
-"	<html>	" +
+"	<html lang='ES'>	" +
+
 "	<head>	" +
+"       <meta charset='utf-8'> " +
+"       <meta http-equiv='X-UA-Compatible' content='IE=edge'> " +
+"       <meta name = 'viewport' content='initial-scale=1, maximum-scale=1'> " +
 "	</head>	" +
+
 "	<body>	" +
-"	<div style='font-family: 'Arial'; '>	" +
-"	<div style='background-color:black;box-sizing: border-box;padding: 20px; width: 100%'>	" +
-"	    <h1 style='color: white; text-align: center;'> TU RESERVA HA SIDO CONFIRMADA</h1>	" +
-"	</div>	" +
+"	    <div style='font-family: 'Arial'; '>	" +
+"	        <div style='background-color:black;box-sizing: border-box;padding: 20px; width: 100%'>	" +
+"	            <h1 style='color: white; text-align: center;'> TU RESERVA HA SIDO CONFIRMADA</h1>	" +
+"	        </div>	" +
 "		" +
-"	<div style='display: flex;'>	" +
+"	        <div style='display: flex;'>	" +
 "		" +
-"	    <div style ='width :60%;'>	" +
+"	            <div style ='width :60%;'>	" +
 "		" +
-"	        <p style='font-weight: bold;    font-size: 30px;    text-align: center;'> %Nombre </p> </br>	" +
+"	                <p style='font-weight: bold;    font-size: 30px;    text-align: center;'> %Nombre </p> </br>	" +
 "		" +
-"	        <p style='text-align: center;    font-size: 20px;'> Ha realizado una reserva para la obra %Obra el %Fecha </br>	" +
-"	            en el teatro %Teatro a las %Hora </p>	" +
+"	                <p style='text-align: center;    font-size: 20px;'> Ha realizado una reserva para la obra %Obra el %Fecha </br>	" +
+"	                en el teatro %Teatro a las %Hora </p>	" +
 "		" +
-"	        <p style='text-align: center;'> Total de reservas    : 	%Total</p>	" +
-"	        <p style='text-align: center;'> Zona               		:	%Zona</p>	" +
-"	        <p style='text-align: center;'> Ubicaciones        	: 	%Ubicacion</p>	" +
-"	        <p style='text-align: center;'> Precio            		: 	%Precio</p>	" +
+"	                <p style='text-align: center;'> Total de reservas    : 	%Total</p>	" +
+"	                <p style='text-align: center;'> Zona               		:	%Zona</p>	" +
+"	                <p style='text-align: center;'> Ubicaciones        	: 	%Ubicacion</p>	" +
+"	                <p style='text-align: center;'> Precio            		: 	%Precio</p>	" +
+"	            </div>	" +
+"		" +
+"	            <div style='width:50%'>	" +
+"	                <img src='cid:%Imagen'>	" +
+"	            </div>	" +
+"		" +
+"	        </div>	" +
+"	        <div>	" +
+"	            <h2 style='color: gray;    text-align: center;'>	" +
+"	            GRACIAS POR LLAMARNOS, NO OLVIDES LLEGAR UNA HORA <br>	" +
+"	            ANTES DE LA FUNCIÓN PARA HACER EFECTIVA TU RESERVA.	" +
+"	            </h2>	" +
+"	            <h3 style='color: gray;    text-align: center;    font-weight: bold;'>	" +
+"	            ¡QUÉ TE DIVIERTAS!	" +
+"	            </h3>	" +
+"	        </div>	" +
 "	    </div>	" +
-"		" +
-"	    <div style='width:50%'>	" +
-"	        <img src='cid:%Imagen'>	" +
-"	    </div>	" +
-"		" +
-"	</div>	" +
-"	<div>	" +
-"	    <h2 style='color: gray;    text-align: center;'>	" +
-"	        GRACIAS POR LLAMARNOS, NO OLVIDES LLEGAR UNA HORA <br>	" +
-"	        ANTES DE LA FUNCIÓN PARA HACER EFECTIVA TU RESERVA.	" +
-"	    </h2>	" +
-"	    <h3 style='color: gray;    text-align: center;    font-weight: bold;'>	" +
-"	        ¡QUÉ TE DIVIERTAS!	" +
-"	    </h3>	" +
-"	</div>	" +
-"	</div>	" +
 "	</body>	" +
 "	</html>	";
         }
